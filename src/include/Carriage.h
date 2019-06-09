5634e207e60f92431997809c9ae7cf08c2b39c05
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-06-09 00:11:17 -0700

    jot down ideas for acceleration function

--- src/include/Carriage.h
+++ src/include/Carriage.h
@@ -16,6 +16,14 @@ public:
                          (config::carriage_pulley_pitch * config::carriage_num_pulley_teeth);
     set_step_freq(step_freq);
   }
+
+  // ... WIP ..., something like this
+  // /* Linearly increase the speed of a motor */
+  // void ramp_up_velocity(const float &init_vel = 0, const float &final_vel, const long_int_type &time)
+  // {
+  //   float delta_vel = final_vel - init_vel;
+  //   set_velocity(delta_vel * (millis() - start_time) / time);
+  // }
 };
 
 #endif
