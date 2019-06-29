3755a095e9f1bc892d95e3bf4097dafb70649697
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-06-28 19:39:28 -0700

    change timing belt pulley pitch to increase precision of carriage motion

--- src/include/Config.h
+++ src/include/Config.h
@@ -8,15 +8,19 @@ typedef unsigned long int long_int_type;
 
 /*                CALIBRATIONS                */
 const float wrap_angle = 45 * 3.14159 / 180; // degrees * pi / 180 = radians
-const float carriage_velocity = 8;           // in/s
+const float carriage_velocity = 5;           // in/s
 const float mandrel_radius = 2;              // inches
 const int_type debounce_time = 50;           // ms
 
 /*                CONSTANTS                */
 /* The number of steps per revolution */
 const int_type steps_per_rev = 200;
+/* - Note that Mandrel motor has 4.25 : 1 gear ratio. The motor itself has 200 steps per
+ * revolution -> 200 * 4.25 = 850 steps per revolution
+ * - Also note the gear box has 1.5 degrees of blacklash error due to gearbox */
+const int_type mandrel_steps_per_rev = 860; // bump up to 860 since usually only 850 steps done @ high speed
 
-const float carriage_pulley_pitch = 0.2;
+const float carriage_pulley_pitch = 0.195f;
 const int_type carriage_num_pulley_teeth = 12;
 
 /*                I/O PINS                */
