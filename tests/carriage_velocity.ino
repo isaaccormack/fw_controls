6a9c708e73ad8f2895bf0adcc12e129fed05b199
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-06-30 00:30:58 -0700

    add some factors to this test case to increase preicion of steps completed over a range of velocities

--- tests/carriage_velocity.ino
+++ tests/carriage_velocity.ino
@@ -28,7 +28,7 @@ Insight: The equation used to set velocity for the carriage uses both large and 
        physically is within 1/16 of an inch of the distance in the software, for various speeds and lengths
 
   Expected Results:
-    - The carriage will move X inches +- 1/16in for various velocities and time lengths
+    - The carriage will move X inches +- 1/16in where X = vel (in/s) * test_time (s) for various velocities and time lengths
 
   LAST EXECUTED RESULTS:
     - (June 28, 2019) The carriage moved to within 1/16 of the position for various velocities and time lengths
@@ -40,33 +40,66 @@ int test_carriage_velocity_()
   Carriage Carriage;
 
   /* START TEST PARAMETERS */
-  // Expect mandrel to move length = vel * test_time
-  // Test 'vel' between 1 -> 6 in/s
-  double vel = 4; // in/s
+  // With gain at 120: For theta = 80deg, vel = 1.5 in/s
+  //                   For theta = 70deg, vel = 1.72 in/s
+  //                   For theta = 20deg, vel = 6 in/s
+  //                   For theta = 15deg, vel = 8 in/s
+  // Thus, optimize between 1.72 -> 6 in/s but test between 1.5 -> 8 in/s
+  double vel = 5; // in/s
   // Test 'test_time' between 1 -> 10 s
-  unsigned long int test_time = 2; // s
+  double test_time = 1; // s
   /* END TEST PARAMETERS */
+
+  // The lag factor is required to compensate for the increased frequency of digital writes at
+  // higher speeds which cummulatively begin to lag the setting the last step time s.t. not enough
+  // steps occur bc last step time is longer than it should be
+  // lag factor associated with cost to read from millis and check condition every loop
+  const unsigned long int LAG_FACTOR = (unsigned long int)(vel + vel / 2);
 
   Carriage.set_velocity(vel);
 
-  unsigned long int start_time = micros();
-  unsigned long int end_time = (test_time * 1000 * 1000) + start_time;
+  const unsigned long int start_time = micros();
+  const unsigned long int end_time = (test_time * 1000 * 1000) + start_time;
 
+  unsigned long int error_flag = 0;
   unsigned long int step_count = 0;
+  unsigned long int counter = 2;
+  unsigned long int prev_counter = 0;
   while (micros() < end_time)
   {
-    if ((micros() - Carriage.get_last_step_time()) > Carriage.get_usec_per_step())
+    // /* Mock Intensive process */
+    // for (int i = 0; i < 20000; ++i)
+    // {
+    //   int k = 0;
+    //   ++k;
+    // }
+    unsigned long int curr_usec = micros();
+    if (curr_usec - Carriage.get_last_step_time() > Carriage.get_usec_per_step())
     {
+      Carriage.set_last_step_time(curr_usec - LAG_FACTOR);
       Carriage.step();
-      Carriage.set_last_step_time(micros());
       ++step_count;
+
+      prev_counter = counter;
+      if (prev_counter == (counter - 1))
+      {
+        // then this for loop has been executed twice in a row (which it should never be)
+        error_flag = 1;
+      }
     }
+    ++counter;
   }
   // Optional debug output
-  // Serial.print((micros() - start_time) / 1000);
-  // Serial.print(" <- Total time taken (ms)\n");
-  // Serial.print(step_count);
-  // Serial.print(" <- Total steps taken\n");
+  if (error_flag > 0)
+  {
+    Serial.print("motor was stepped twice in a row due to lag factor\n");
+  }
+  Serial.print((micros() - start_time) / 1000);
+  Serial.print(" <- Total time taken (ms)\n");
+  Serial.print(step_count);
+  Serial.print(" <- Total steps taken\n");
+  Serial.print(counter);
+  Serial.print(" <- Total loops\n");
   return 0;
 }
 
