6c527d0d955ee1e3d84fb25c25c220d94c7c5eee
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-07-01 21:36:56 -0700

    snapshot comments

--- src/include/Mandrel.h
+++ src/include/Mandrel.h
@@ -12,9 +12,7 @@ public:
 
   void set_velocity(const double &tan_velocity)
   {
-    /* T = r / v * step_per_rev,
-     * => us/step = 1000 * 1000 * TWO_PI * Mr / (Vm,tan * mandrel steps per rev) */
-    long_int_type usec_per_step = 1000000;
+    long_int_type usec_per_step = 1000000; // convert s to us
     usec_per_step *= TWO_PI;
     usec_per_step *= config::mandrel_radius;
     usec_per_step /= (tan_velocity);
@@ -22,6 +20,32 @@ public:
 
     set_usec_per_step(usec_per_step);
   }
+
+  // void set_filament_offset_delay_time(const double &tan_velocity)
+  // {
+  //   long_int_type filament_offset_delay_time_ = 1000000; // convert s to us
+  //   filament_offset_delay_time_ *= config::filament_width;
+  //   filament_offset_delay_time_ *= (1.0 - config::filament_overlap_percent);
+  //   filament_offset_delay_time_ /= tan_velocity;
+  //   filament_offset_delay_time_ /= 2.0; // divide by 2 because delay occurs on both sides
+  // }
+
+  // long_int_type get_filament_offset_delay_time()
+  // {
+  //   return filament_offset_delay_time_;
+  // }
+
+  // void set_rev_start_time(const long_int_type &rev_start_time) { rev_start_time_ = rev_start_time; }
+  // long_int_type get_rev_start_time() { return rev_start_time_; }
+
+  void inc_step_count() { ++step_count_; }
+  void clear_step_count() { step_count_ = 0; }
+  void get_step_count() { return step_count_; }
+
+private:
+  long_int_type filament_offset_delay_time_ = 0; // init as 0 for now...
+  long_int_type rev_start_time_ = 0;             // init as 0 for now...
+  long_int_type step_count_ = 0;                 // init as 0 for now...
 };
 
 #endif
