5771a0110d862832a42dbbb9821ce883749b6e75
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-07-01 18:39:28 -0700

    wip with the delay period on both ends to account for filament percent overlap, the method here does not take into account ffeedback from limit switch, UNTESTED

--- src/main.ino
+++ src/main.ino
@@ -14,12 +14,14 @@ int main_()
 
   Mandrel Mandrel;
   Mandrel.set_velocity(mandrel_velocity);
+  Mandrel.set_filament_offset_delay_time(mandrel_velocity);
 
   Switch C_Home_Switch(config::c_home_switch_pin, Carriage);
   Switch C_Far_Switch(config::c_far_switch_pin, Carriage);
+  Switch M_Encoder_Switch(config::m_encoder_switch_pin, Mandrel);
 
   Motor *Motors[2] = {&Mandrel, &Carriage};
-  Switch *Dir_Switches[2] = {&C_Home_Switch, &C_Far_Switch};
+  Switch *Carriage_Switches[2] = {&C_Home_Switch, &C_Far_Switch};
 
   // Carriage.home();
 
@@ -32,7 +34,7 @@ int main_()
       {
         if (m->is_dir_flip_flag_set())
         {
-          if (curr_usec - m->get_last_dir_flip_time() < config::dir_flip_timeout_dur)
+          if (curr_usec - m->get_last_dir_flip_time() < Mandrel.get_filament_offset_delay_time())
           {
             continue;
           }
@@ -43,14 +45,19 @@ int main_()
       }
     }
 
-    for (Switch *s : Dir_Switches)
+    for (Switch *s : Carriage_Switches)
     {
       if (s->is_rising_edge() && !s->motor.is_dir_flip_flag_set())
       {
+        // Set this first to resemble closest possible time to when limit switch was hit
+        s->motor.set_last_dir_flip_time(micros());
         s->motor.flip_dir();
         s->motor.set_dir_flip_flag();
-        s->motor.set_last_dir_flip_time(micros());
       }
+    }
+    if (M_Encoder_Switch.is_rising_edge())
+    {
+      Mandrel.set_rev_start_time(micros());
     }
   }
   return 0;
