5ec875c45d731ca148bf5f87cded58b465ee2fbc
Isaac Cormack <i@Isaacs-MBP.hitronhub.home>
2019-08-11 14:56:59 -0700

    update mandrel velocity test case to account for testing of the mandrel motor at wrap angles which cause resonant frequencies, test run with no problems after calibration

--- tests/mandrel_velocity/mandrel_velocity.ino
+++ tests/mandrel_velocity/mandrel_velocity.ino
@@ -13,13 +13,19 @@
            implementation
 
   Procedure:
-    1. Decide on a range of tan_vel and revolutions values to execute this test case over
-       depending on whether this test case is being used for sanity or calibration.
-    2. Line the keyway of the drive shaft parallel with eye level s.t. the plane is barely not visible
-    3. Execute the test case as to rotate the mandrel X times while leaving eye line in same orientation
-    4. Assert that the mandrel stops in the same position as it was initially after X rotations by seeing that the
+    1. Execute this test over the range of values at which the mandrel will rotate at according to
+       the wrap angle.
+    2. Ensure this test is run outside of the wrap angle which cause a resonant frequency in the mandrel 
+       as well as within the wrap angle which causes a resonant frequency but with the carriage velocity
+       properly calibrated as to make the mandrel rotate slower s.t. the motor does not incur a resonant
+       frequency (and especially near boundaries speeds over which this calibration effects). This is to
+       validate no steps are missed due to inaccurate calibration near the resonant frequency wrap angle.
+       Details on calibrating this carriage velocity to compensate for this are found in the config file.
+    3. Line the keyway of the drive shaft parallel with eye level s.t. the plane is barely not visible
+    4. Execute the test case as to rotate the mandrel X times while leaving eye line in same orientation
+    5. Assert that the mandrel stops in the same position as it was initially after X rotations by seeing that the
        flat on the drive shaft is still parallel with your line of sight.
-    5. (Optional) Calibrate required parameters itteratively until the precision of the mandrels rotation is within 1 - 3 steps
+    6. (Optional) Calibrate required parameters itteratively until the precision of the mandrels rotation is within 1 - 3 steps
 
   Expected Results:
     - The mandrel will rotate exactly X rotations as specified in the software under various mandrel
@@ -34,6 +40,10 @@
     - (June 31, 2019) Update range of test velocities to 3.5 -> 10 due to mandrel slipping with speeds > 10in/s
       (and even at 10in/s). Given the machine operates at > 10in/s there would need to be acceleration into this
       to keep system accurate, note this for later development if tan_vel > 10in/s is necessary.
+    - (Aug. 10, 2019) Update test to use mandrel velocity as calculated in 2 axis instead of manual input which 
+      slightly redefines purpose of test. Test run with new calibration to compensate for mandrel velocity resonant
+      frequency between 52 and 60 degree wrap angle. With the current calibration the motor has accurate velocity
+      and does not skips steps at wrap angles in and outside of the values which otherwise cause a resonant frequency.
  */
 
 typedef config::int_type int_type;
@@ -45,18 +55,19 @@ int test_mandrel_velocity_()
   Mandrel Mandrel;
 
   /* START TEST PARAMETERS */
-  // 3.5 <= tan_vel <= 10 in/s -- for > 10in/s, results are comprimised by slippage of motor due to fast acceleration
-  const double tan_vel = 10;
+  // config::deg_wrap_angle between 15 and 80 degrees
   // 1 <= revolutions <= 10
-  const long_int_type revolutions = 2;
+  const long_int_type revolutions = 3;
   /* END TEST PARAMETERS */
 
-  double ang_vel = (tan_vel / TWO_PI) / config::mandrel_radius;
+  /* Vm,tan / Vc = tan(wrap_angle)     =>     Vm,tan = Vc * tan(wrap_angle) */
+  double mandrel_velocity = config::carriage_velocity * tan(config::wrap_angle);
+  Mandrel.set_velocity(mandrel_velocity);
+
+  double ang_vel = (mandrel_velocity / TWO_PI) / config::mandrel_radius;
 
   // Set test time to time required to do the number of defined revolutions
   const long_int_type test_time = (1000000 * revolutions) / ang_vel; // 1,000,000 us = 1s
-
-  Mandrel.set_velocity(tan_vel);
 
   const long_int_type start_time = micros();
   const long_int_type end_time = test_time + start_time;
